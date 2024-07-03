241a698af31cce9bb8e0c69d15e2c000e96a39ed
Björn Tenje Persson <bjorn@tenje.se>
2024-07-03 11:51:42 +0200

    removed duplicate code

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -4,10 +4,10 @@ vim.keymap.set("n", "<leader>q", "<cmd>confirm q<CR>", { desc = "Quit ", silent 
 vim.keymap.set("n", "<leader>w", ":w!<CR>", { desc = "Write", silent = true })
 
 -- Always use soft move in normal and visual mode
-vim.api.nvim_set_keymap("n", "k", "gk", { noremap = true, silent = true, silent = true })
-vim.api.nvim_set_keymap("n", "j", "gj", { noremap = true, silent = true, silent = true })
-vim.api.nvim_set_keymap("v", "j", "gj", { noremap = true, silent = true, silent = true })
-vim.api.nvim_set_keymap("v", "k", "gk", { noremap = true, silent = true, silent = true })
+vim.api.nvim_set_keymap("n", "k", "gk", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "j", "gj", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "j", "gj", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "k", "gk", { noremap = true, silent = true })
 
 -- Move row up and down in normal, insert and visual mode.
 vim.api.nvim_set_keymap("n", "<A-j>", ":m .+1<CR>==", { noremap = true, silent = true })
@@ -34,7 +34,6 @@ vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Search by Grep" }
 vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Search Diagnostics" })
 vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Search Resume" })
 vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = 'Search Recent Files ("." for repeat)' })
--- vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
 
 -- Formatting
 vim.keymap.set({ "n", "v" }, "<leader>cf", function()
