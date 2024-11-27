155b7a2ec5926bf251d3563c9dfce09b18617616
Björn Tenje Persson <bjorn@tenje.se>
2024-11-27 11:42:25 +0100

    Snippets
    Riddle of strider

--- lua/my_snippets/all.lua
+++ lua/my_snippets/all.lua
@@ -6,6 +6,20 @@ ls.add_snippets("all", {
 	s("loremh", {
 		t({ "Lorem Ipsum", "" }),
 	}),
+	s("loreml", {
+		t({
+			"All that is gold does not glitter,",
+			"Not all those who wander are lost;",
+			"The old that is strong does not wither,",
+			"Deep roots are not reached by the frost.",
+			"",
+			"From the ashes a fire shall be woken,",
+			"A light from the shadows shall spring;",
+			"Renewed shall be blade that was broken,",
+			"The crownless again shall be king.",
+			"",
+		}),
+	}),
 	s("loremr", { -- Trigger is 'log'
 		t({ "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit", "" }),
 	}),
@@ -29,6 +43,17 @@ ls.add_snippets("all", {
 		}),
 		t({
 			"Phasellus et commodo velit. Nulla sit amet semper odio, a suscipit est. Praesent tincidunt vulputate leo, a pharetra dui cursus non. Praesent vitae sem in justo egestas eleifend in quis augue. Praesent ac sem ligula. Proin efficitur interdum diam, non lobortis magna feugiat a. Phasellus eu ante faucibus, venenatis lectus nec, scelerisque risus. Phasellus vitae consequat velit. Integer eu urna aliquam, bibendum felis eget, tristique nunc. Sed rutrum eleifend tortor, sed dignissim nisl sagittis in.",
+			"",
+			"",
+		}),
+		t({
+			"Praesent eu dignissim orci. Aenean id pulvinar tortor, eu varius neque. Vestibulum nibh nibh, dapibus at accumsan nec, commodo ac ante. Pellentesque condimentum porttitor leo sed consequat. Nulla facilisi. Nulla dapibus eu lorem sit amet commodo. Duis blandit sit amet libero vel sodales. Aenean dignissim dui vel eleifend faucibus. In ante erat, varius nec scelerisque in, mattis sit amet quam. Sed tincidunt odio facilisis orci aliquam dictum a non justo. Sed molestie cursus dolor eget mattis. Nam in pellentesque libero. Nulla vitae ante euismod, maximus leo ac, imperdiet odio.",
+			"",
+			"",
+		}),
+		t({
+			"Cras sed purus elit. Fusce vitae malesuada massa, convallis placerat libero. Ut in iaculis lorem. Mauris rhoncus pellentesque mauris, ut molestie diam consequat nec. Morbi dapibus egestas aliquam. Nulla facilisi. Etiam id dolor nec est luctus condimentum id id tortor. Aliquam varius, enim vel gravida faucibus, elit lacus mattis quam, a molestie mauris augue sagittis quam. Sed aliquet, diam quis mollis suscipit, ex est hendrerit magna, at porttitor nulla augue ut lectus. Fusce tempus ipsum sed sollicitudin condimentum. Praesent nisl magna, bibendum nec odio nec, suscipit faucibus dolor. Nullam nec odio nibh. Etiam id porttitor lectus. Duis placerat libero at congue posuere. Quisque elementum malesuada mauris, quis dictum turpis. Suspendisse ornare lacus at ante lobortis, vel pharetra ipsum dictum.",
+			"",
 			"",
 		}),
 	}),
