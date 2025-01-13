33d1c45bbed0e4180da0e9c9106b2368813c2d89
Björn Tenje Persson <bjorn@tenje.se>
2025-01-13 18:04:17 +0100

    Removed comments

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,8 +1,8 @@
 return {
 	{
 		"folke/which-key.nvim",
-		event = "VimEnter", -- Sets the loading event to 'VimEnter'
-		config = function() -- This is the function that runs, AFTER loading
+		event = "VimEnter",
+		config = function()
 			require("which-key").setup()
 		end,
 		keys = {
